79323af0da3253c862bdfcaa37ca3ed2fe024be7
medkhalilbk <medkhalilbenkhelil@gmail.com>
2024-08-13 00:25:45 +0100

    commit

--- src/types/DeliveryMan.ts
+++ src/types/DeliveryMan.ts
@@ -1,12 +1,13 @@
 export interface IDeliveryMan {
-    id: string;
-    fullName: string;
-    phoneNumber: string;
-    socialStatus?: string;
-    ordersCompleted: number;
-    vehiculeSerialNumber?: string;
-    cin: string;
-    isActive: boolean;
-    contractFile?: string;
-    userInfo?:any
-  }
+  id: string;
+  fullName: string;
+  phoneNumber: string;
+  socialStatus?: string;
+  ordersCompleted: number;
+  vehiculeSerialNumber?: string;
+  cin: string;
+  isActive: boolean;
+  contractFile?: string;
+  userInfo?: any;
+  isDeleted: boolean
+}
