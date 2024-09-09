69b278d7cdc770ff76fddd505bf817ea4a1a6bcf
medkhalilbk <medkhalilbenkhelil@gmail.com>
2024-09-09 06:39:43 +0100

    update key 2

--- src/lib/notifications/firebase.ts
+++ src/lib/notifications/firebase.ts
@@ -1,9 +1,8 @@
 
-import * as admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json';
+import * as admin from 'firebase-admin'; 
 import { getApps } from 'firebase-admin/app';
 let alreadyCreatedApps = getApps()
-
+let serviceAccount = require("./serviceAccountKey.json")
 let firebase  = alreadyCreatedApps.length == 0 ? admin.initializeApp({
     credential:admin.credential.cert(serviceAccount as any)}) : alreadyCreatedApps[0]  as any; 
  
